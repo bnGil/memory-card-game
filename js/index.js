c5b3350aede80f395440d2691a4448997a5c539c
Mor Menashe <mormen1305@gmail.com>
2022-05-07 22:07:30 +0300

    updated design and themes

--- js/index.js
+++ js/index.js
@@ -2,12 +2,18 @@ const open = document.getElementById("open");
 const modalContainer = document.getElementById("modal-container");
 const close = document.getElementById("close");
 
-const select = document.getElementById("selectbox");
-select.addEventListener("change", () => {
-  sessionStorage.setItem(
-    "difficulty",
-    select.options[select.selectedIndex].value
-  );
+const difficulties = document.querySelectorAll(".difficulty");
+difficulties.forEach((difficulty) => {
+  difficulty.addEventListener("click", (event) => {
+    sessionStorage.setItem("difficulty", event.target.value);
+  });
+});
+
+const themes = document.querySelectorAll(".theme");
+themes.forEach((theme) => {
+  theme.addEventListener("click", (event) => {
+    sessionStorage.setItem("theme", event.target.value);
+  });
 });
 
 open.addEventListener("click", () => {
